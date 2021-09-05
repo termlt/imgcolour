b3f8c8f824b16a0b43ce5bdb86fc020f17329c90
termlt <75373156+termlt@users.noreply.github.com>
2021-09-05 14:43:31 +0400

    Add files via upload

---
+++ imgcolour.py
@@ -0,0 +1,16 @@
+import cv2
+
+def colorp(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cBGR = image[y, x]
+        cRGB=tuple(reversed(cBGR))
+
+        print(f"RGB Value: {cRGB} \n")
+
+
+image = cv2.imread("barack.jpg")  #Change the picture to yours
+
+cv2.namedWindow('Color Picker')
+cv2.setMouseCallback('Color Picker', colorp)
+cv2.imshow('Color Picker', image)
+cv2.waitKey(0)

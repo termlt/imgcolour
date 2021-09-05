7e9434db761df6c7da01610b65fab3b7eed0910f
termlt <75373156+termlt@users.noreply.github.com>
2021-09-05 21:26:31 +0400

    New functions

--- imgcolour.py
+++ imgcolour.py
@@ -1,17 +1,27 @@
 import cv2
+import json
+import requests
 
 def colorp(event, x, y, flags, param):
+    
     if event == cv2.EVENT_LBUTTONDOWN:
         cBGR = image[y, x]
         cRGB=tuple(reversed(cBGR))
 
-        print(f"RGB code: {cRGB} \n")
+        r = requests.get(f'https://www.thecolorapi.com/id?rgb=rgb{cRGB}').json()
+        colour_rex = r['hex']['value']
+        colour_name = r['name']['value']
+
+        print(f'Colour name: {colour_name}')
+        print(f'HEX: {colour_rex}')
+        print(f'RGB code: {cRGB} \n')
 
 
-img = cv2.imread("barack.jpg")  #Change the picture to yours
-image = cv2.resize(img, (800, 600))
+img = cv2.imread('family.jpg')
+image = cv2.resize(img, (800, 600))  #Change the picture to yours
 
-cv2.namedWindow('Color Picker')
-cv2.setMouseCallback('Color Picker', colorp)
-cv2.imshow('Color Picker', image)
+
+cv2.namedWindow('imgcolour')
+cv2.setMouseCallback('imgcolour', colorp)
+cv2.imshow('imgcolour', image)
 cv2.waitKey(0)
